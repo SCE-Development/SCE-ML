22e19fe7220fe40f326b33c81d0146f42f561d29
Isaac Chan <isaacchancollege@gmail.com>
2023-02-02 17:20:36 -0800

    add file rank mapping and piece bitboard

--- CHESS-AI/chessboard.py
+++ CHESS-AI/chessboard.py
@@ -6,7 +6,17 @@ class Board:
     # and a variable, board, which is a tuple with numbers 1-64
     # used to calculate possible piece moves
     def __init__(self):
-        
+        self.fileDict = {
+            "0": "a",
+            "1": "b",
+            "2": "c",
+            "3": "d",
+            "4": "e",
+            "5": "f", 
+            "6": "g", 
+            "7": "h"
+            }
+
         self.fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
         self.board = [
             "R", "N", "B", "K", "Q", "B", "N", "R",
@@ -46,16 +56,40 @@ class Board:
                     self.bitboards[key] = self.bitboards[key] << 1 | 0b1
                 else:
                     self.bitboards[key] = self.bitboards[key] << 1
+
+
     def pawnMoveGen(self):
         pawnMoves = self.bitboards["p"] << 8 | self.bitboards["P"] >> 8
         print(format(pawnMoves, "064b"))
+    
+    def getFileRank(self, index):
+        file = self.fileDict[str(index % 8)]
+        if index < 8:
+            return file + "1"
+        elif index < 16:
+            return file + "2"
+        elif index < 24:
+            return file + "3"
+        elif index < 32:
+            return file + "4"
+        elif index < 40:
+            return file + "5"
+        elif index < 48:
+            return file + "6"
+        elif index < 56:
+            return file + "7"
+        else:
+            return file + "8"
+
+    def pieceBitboard(self, index):
+        return 0b1 << index
+
 
 brd = Board()
 brd.board2Bitboard()
+print(brd.getFileRank(8))
 
 for key in brd.bitboards.keys():
     print(key + ": " + format(brd.bitboards[key], "064b"))
-
-
 
 brd.pawnMoveGen()
