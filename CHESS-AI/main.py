dff97608cf08832db5a7e6d5ef68d27f7ca9955a
Isaac Chan <isaacchancollege@gmail.com>
2023-03-24 20:22:28 -0700

    set default value of lookingForward for makeMove to False

--- CHESS-AI/main.py
+++ CHESS-AI/main.py
@@ -15,14 +15,13 @@ while play:
         print("Turn for White")
         while not validMove:
             move = input()
-            start, end = move[0:2], move[2:4]
             # Valid move inputs look like "b2b3" where the first two characters of the string of length 4
             # specify the starting square and the last two characters specify the ending square.
             if len(move) < 4 or len(move) > 4 or move[0:2] not in brd.fileRank2index or move[2:4] not in brd.fileRank2index or brd.board[brd.fileRank2index[move[0:2]]].islower():
                 print("PLEASE ENTER A VALID MOVE")
             else:
                 start, end = brd.fileRank2index[move[0:2]], brd.fileRank2index[move[2:4]]
-                if brd.makeMove(start, end, False):
+                if brd.makeMove(start, end):
                     validMove = True
         brd.printBoard()
         color = False
@@ -31,14 +30,13 @@ while play:
         print("Turn for Black")
         while not validMove:
             move = input()
-            start, end = move[0:2], move[2:4]
             # Valid move inputs look like "b2b3" where the first two characters of the string of length 4
             # specify the starting square and the last two characters specify the ending square.
             if len(move) < 4 or len(move) > 4 or move[0:2] not in brd.fileRank2index or move[2:4] not in brd.fileRank2index or brd.board[brd.fileRank2index[move[0:2]]].isupper():
                 print("PLEASE ENTER A VALID MOVE")
             else:
                 start, end = brd.fileRank2index[move[0:2]], brd.fileRank2index[move[2:4]]
-                validMove = brd.makeMove(start, end, False)
+                validMove = brd.makeMove(start, end)
         brd.printBoard()
         color = True
         validMove = False
